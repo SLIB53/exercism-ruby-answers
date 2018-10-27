79371465475f5a058cb9c74f4c251e4a052bf7e8
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-10-26 20:32:57 -0500

    Add run all tests sh script

---
+++ test_all.sh
@@ -0,0 +1,17 @@
+#!/bin/env sh
+
+# Runs tests for each exercise, exiting with 1 if a test fails. Tests are found
+# by convention, where the test is ruby script ending with "_test.exs".
+
+for item in $(find . -name "*_test.rb" | sort)
+do
+    echo "\n\033[34m> ruby $item\033[0m"
+
+    if ! ruby $item;
+    then
+        echo "\n\033[31mA test in $item failed.\033[0m"
+        exit 1
+    fi
+done
+
+echo "\n\033[32mAll tests passed!\033[0m"
