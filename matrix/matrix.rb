e1c855277d31c904a76004cf0f1dad0d81391916
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-11-04 21:47:49 -0600

    Refactor matrix, use readonly properties

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,15 +1,10 @@
 class Matrix
   def initialize(serial)
-    @serial = serial
+    serialized_rows = serial.split("\n")
+    @rows = serialized_rows.map { |s_row| s_row.split.map { |s_num| Integer(s_num) } }
+    @columns = rows.transpose
   end
 
-  def rows
-    serialized_rows = @serial.split("\n")
-    serialized_rows.map { |s_row| s_row.split.map { |s_num| Integer(s_num) } }
-  end
-
-  def columns
-    deserialized_rows = rows
-    deserialized_rows[0].zip(*deserialized_rows[1..-1])
-  end
+  attr_reader :rows
+  attr_reader :columns
 end
