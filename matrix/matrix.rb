e2c2feaeec2509417c20fa445fa8e33e9592ae68
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-11-05 03:14:00 -0600

    Minor refactor matrix, code cleanup

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,8 +1,7 @@
 class Matrix
   def initialize(serial)
-    serialized_rows = serial.split("\n")
-    @rows = serialized_rows.map { |s_row| s_row.split.map { |s_num| Integer(s_num) } }
-    @columns = rows.transpose
+    @rows = serial.split("\n").map { |s_row| s_row.split.map { |s_num| Integer(s_num) } }
+    @columns = @rows.transpose
   end
 
   attr_reader :rows
