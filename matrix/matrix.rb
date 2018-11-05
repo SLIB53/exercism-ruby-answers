6f0e4b87bfe1880320fd6797c1c0d270654eb646
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-11-05 14:36:25 -0600

    Refactor matrix, extract and cleanup

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,9 +1,18 @@
 class Matrix
+  attr_reader :rows, :columns
+
   def initialize(serial)
-    @rows = serial.split("\n").map { |s_row| s_row.split.map { |s_num| Integer(s_num) } }
+    @rows = deserialize_rows(serial)
     @columns = @rows.transpose
   end
 
-  attr_reader :rows
-  attr_reader :columns
+  private
+
+  def deserialize_rows(serial)
+    serial.split("\n").map { |s_row| deserialize_row(s_row) }
+  end
+
+  def deserialize_row(row_serial)
+    row_serial.split.map { |s_num| Integer(s_num) }
+  end
 end
